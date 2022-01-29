0da9634e228c21e1440c78c10ed9ab79ad697985
Jimmy Rando <jsturat46925@gmail.com>
2022-01-29 17:18:47 -0500

    added gitignore and started inquirer setup

--- app.js
+++ app.js
@@ -1,18 +1,30 @@
-const fs= require('fs');
+const inquirer= require('inquirer');
+
+inquirer
+    .prompt
+    ([
+        {
+            type: 'input',
+            name: 'name',
+            message: 'What is your name?'
+        }
+    ])
+    .then(answers=> console.log(answers));
+/*const fs= require('fs');
 const generatePage= require('./src/page-template.js');
 
 
-const profileDataArgs= process.argv.slice(2);
-const [name, github]= profileDataArgs;
+//const profileDataArgs= process.argv.slice(2);
+//const [name, github]= profileDataArgs;
+const pageHTML= generatePage(name, github);
 
 
-
-fs.writeFile('./index.html', generatePage(name, github), err => 
+fs.writeFile('./index.html', pageHTML, err => 
 {
     if (err) throw new Error(err);
 
     console.log('Portfolio complete! Check out index.html to see the output!');
-});
+});*/
 
 
 //console.log(name, github);
